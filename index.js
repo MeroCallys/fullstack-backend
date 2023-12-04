d04935fbd94ce058cd41af6c207aa94f3977a7d4
MeroCallys <MeroCallys@gmail.com>
2023-12-03 21:43:55 -0700

    Version 0: HTTP GET AND POST

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// import cors from "cors";
+import cors from "cors";
 import express from "express";
-import bodyParser from "body-parser";
 import pg from "pg";
-import Chance from "chance";
 
 const postgres = {
   user: "postgres",
@@ -12,80 +10,68 @@ const postgres = {
   port: 5432,
 };
 
-const db = new pg.Client({
-  user: postgres.user,
-  host: postgres.host,
-  database: postgres.database,
-  password: postgres.password,
-  port: postgres.port,
-});
+const pool = new pg.Pool(postgres);
 
+// initialize
 const app = express();
 const port = 3000;
-const chance = new Chance();
-db.connect();
 
-let dbData;
+// pool connection
+await pool.connect();
 
-console.log(typeof chance.name());
-
-db.query("SELECT * FROM profiles", (err, res) => {
-  if (err) console.log("Error executing query", err.stack);
-  else dbData = res.rows;
-  db.end();
+// notification
+pool.on("error", (err) => {
+  console.error("something bad has happened!", err.stack);
 });
 
-// for (let i = 0; i < 1; i++) {
-//   db.query(
-//     `INSERT INTO profiles(id, category,company,first_name,last_name,street,city,postal_code,province,country,phone,email,created_on)
-//     VALUES (
-//       $1,
-//       $2,
-//       $3,
-//       $4,
-//       $5,
-//       $6,
-//       $7,
-//       $8,
-//       $9,
-//       $10,
-//       $11,
-//       $12,
-//       $13
-//     ) `,
-//     [
-//       "DEFAULT",
-//       "walk-in",
-//       "walk-in",
-//       `${chance.first()}`,
-//       `${chance.last()}`,
-//       `${chance.street()}`,
-//       `${chance.city()}`,
-//       `${chance.postcode()}`,
-//       `${chance.province()}`,
-//       `${chance.country()}`,
-//       `${chance.phone()}`,
-//       `${chance.email()}`,
-//       "LOCALTIMESTAMP",
-//     ],
-//     (err, res) => {
-//       if (err) console.log("Error executing query", err.stack);
-//       else dbData = res.rows;
-//       db.end();
-//     }
-//   );
-// }
-
-// app.use(cors);
-
 // middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/profiles", (req, res) => {
-  res.json(dbData);
-  console.log(dbData);
+app.get("/ids", async (req, res) => {
+  const result = await pool.query("SELECT id FROM profiles");
+  res.json(result.rows);
+});
+app.get("/ids", async (req, res) => {
+  const result = await pool.query("SELECT id FROM profiles");
+  res.json(result.rows);
+});
+
+app.get("/fullnames", async (req, res) => {
+  const result = await pool.query(
+    "SELECT first_name as firstName, last_name as lastName FROM profiles"
+  );
+  res.json(result.rows);
+});
+
+app.get("/profiles", async (req, res) => {
+  const result = await pool.query("SELECT * FROM profiles");
+  res.send(result.rows);
+});
+
+app.post("/add-profile", async (req, res) => {
+  const input = Object.values(req.body);
+  await pool.query(
+    `INSERT INTO profiles (id, category, company, first_name, last_name, street, city, postal_code, province, country, phone, email, created_on)
+    VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, CURRENT_TIMESTAMP)`,
+    input
+    // [
+    //   "Dealership",
+    //   "GMC Wolfe",
+    //   "Rose",
+    //   "Seign",
+    //   "793 Tamarack Way",
+    //   "Edmonton",
+    //   "T6T 2M4",
+    //   "Alberta",
+    //   "Canada",
+    //   "780-265-6949",
+    //   "lhey123@gmail.com",
+    // ]
+  );
 });
 
 app.listen(port, () => {
-  console.log(`Server is up and running at  http://localhost:${port}`);
+  console.log(`Server is up and running at http://localhost:${port}`);
 });
